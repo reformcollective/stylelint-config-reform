9ef4177e8a9ff69dcc1610966fc21e13993e3119
Robbie <robbiethewadley@gmail.com>
2023-04-24 11:32:59 -0600

    pull in config

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: "stylelint-config-standard",
+  customSyntax: "postcss-styled-syntax",
+  overrides: [
+    {
+      files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    },
+  ],
+  plugins: ["stylelint-no-unsupported-browser-features"],
+  rules: {
+    "no-empty-source": null,
+    "block-no-empty": null,
+    "selector-type-no-unknown": null,
+    "plugin/no-unsupported-browser-features": [
+      true,
+      {
+        severity: "warning",
+        ignorePartialSupport: true,
+      },
+    ],
+  },
+};
