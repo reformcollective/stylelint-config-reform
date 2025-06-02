5cdbada7d8aa5cb500fb0a26f267528c208a52b2
Robbie <robbiethewadley@gmail.com>
2025-06-02 16:49:49 -0600

    testing baseline

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 module.exports = {
-  extends: "stylelint-config-standard",
-  customSyntax: "postcss-styled-syntax",
-  overrides: [
-    {
-      files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    },
-  ],
-  plugins: ["stylelint-no-unsupported-browser-features"],
-  rules: {
-    "no-empty-source": null,
-    "block-no-empty": null,
-    "selector-type-no-unknown": null,
-    "media-query-no-invalid": null,
-    "media-feature-range-notation": null,
-    "custom-property-empty-line-before": null,
-    "plugin/no-unsupported-browser-features": [
-      true,
-      {
-        severity: "warning",
-        ignore: ["css-nesting", "css3-cursors"],
-        ignorePartialSupport: true,
-      },
-    ],
-  },
+	extends: "stylelint-config-standard",
+	customSyntax: "postcss-styled-syntax",
+	overrides: [{ files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"] }],
+	plugins: [
+		"stylelint-no-unsupported-browser-features",
+		"stylelint-plugin-use-baseline",
+	],
+	rules: {
+		"no-empty-source": null,
+		"block-no-empty": null,
+		"selector-type-no-unknown": null,
+		"media-query-no-invalid": null,
+		"media-feature-range-notation": null,
+		"custom-property-empty-line-before": null,
+		"plugin/no-unsupported-browser-features": [
+			true,
+			{
+				severity: "warning",
+				ignore: ["css-nesting", "css3-cursors"],
+				ignorePartialSupport: true,
+			},
+		],
+		"plugin/use-baseline": [true, { available: "widely" }],
+	},
 };
